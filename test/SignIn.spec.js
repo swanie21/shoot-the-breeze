588328dde172f1f60e8b55b6db7e0fab074c1e57
Ben Godfrey <bcgodfrey91@gmail.com>
2016-09-21 23:17:23 -0600

    Finish initial testing.

--- test/SignIn.spec.js
+++ test/SignIn.spec.js
@@ -1,65 +1,32 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import { expect, assert } from 'chai';
-import NavBar from '../lib/components/NavBar';
-
-describe('NavBar component',function() {
-
-  const wrapper = shallow(<NavBar />);
-
-  it('should be rendered into a nav container', function() {
-    expect(wrapper.type(), 'nav');
-  });
-
-  it('should have a sort buttons container', function() {
-    expect(wrapper.find('.SortButtons')).to.have.length(1);
-  });
-
-  it('should have messages prop', function() {
-    expect(wrapper.props().messages).to.be.defined;
- });
-
- it('should have filterText prop', function() {
-   expect(wrapper.props().filterText).to.be.defined;
-});
-
-it('should have updateFilter prop', function() {
-  expect(wrapper.props().updateFilter).to.be.defined;
-});
-
-it('should have sortUpButton prop', function() {
-  expect(wrapper.props().sortUpButton).to.be.defined;
-});
-
-it('should have sortDownButton prop', function() {
-  expect(wrapper.props().sortDownButton).to.be.defined;
-});
-
-});
-
-describe('render',function() {
-
-  const wrapper = shallow(<NavBar />);
-
-  it('has a filter', () => {
-     assert.lengthOf(wrapper.find('Filter'), 1);
-   });
-
-   it('has a sort up button', () => {
-      assert.lengthOf(wrapper.find('SortUpButton'), 1);
-    });
-
-   it('has a sort down button', () => {
-       assert.lengthOf(wrapper.find('SortDownButton'), 1);
-   });
-
-   it('should have attributes for className, filterText, updateFilter, allMessages, sortUpButton, and sortDownButton', function() {
-     expect(wrapper.props('className')).to.be.defined;
-     expect(wrapper.props('filterText')).to.be.defined;
-     expect(wrapper.props('updateFilter')).to.be.defined;
-     expect(wrapper.props('allMessages')).to.be.defined;
-     expect(wrapper.props('sortUpButton')).to.be.defined;
-     expect(wrapper.props('sortDownButton')).to.be.defined;
-   });
-
-});
+// import React from 'react';
+// import { shallow, mount, render } from 'enzyme';
+// import { expect, assert } from 'chai';
+// import SignIn from '../lib/components/SignIn';
+//
+// describe('SignIn component',function() {
+//
+//   const wrapper = shallow(<SignIn />);
+//
+//   it('should be rendered into a section container', function() {
+//     expect(wrapper.type(), 'section');
+//   });
+//
+//   it('should have messages prop', function() {
+//     expect(wrapper.props().user).to.be.defined;
+//  });
+//
+// });
+//
+// describe('render',function() {
+//
+//   const wrapper = shallow(<SignIn />);
+//
+//   it('has a LoginButton', () => {
+//      assert.lengthOf(wrapper.find('LoginButton'), 1);
+//    });
+//
+//    it('should have attributes for className', function() {
+//      expect(wrapper.props('className')).to.be.defined;
+//    });
+//
+// });
